f40f2421e3203cef5b51cc67719c524060faa4de
Maximilian Burkart <maxiburkart@gmail.com>
2020-07-13 10:37:49 +0200

    Add setup script for shape production.

---
+++ utils/setup_root.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
+source /home/wunsch/workspace/root/build_own_python/bin/thisroot.sh
+
+alias python=/home/wunsch/workspace/python/install/bin/python3.6
+export PYTHONPATH=$PWD:$PYTHONPATH
+export PYTHONPATH=$PWD/Dumbledraw:$PYTHONPATH
