99076d8f3c7278d4c1de92020e1be87b2e47db3c
Maximilian Burkart <maxiburkart@gmail.com>
2020-07-23 20:01:23 +0200

    Fix bugs in conversion to synced shapes.
    Additionally add option to write the final histograms in a single
    variable if multiple are present in the root file. Add log messages.

--- shapes/convert_to_synced_shapes.py
+++ shapes/convert_to_synced_shapes.py
@@ -23,7 +23,7 @@ _process_map = {
     "jetFakes": "jetFakes",
     "jetFakesMC": "jetFakes",
     "QCD": "QCD",
-    "QCDMC": "QCD",
+    "QCD": "QCDMC",
 }
 
 def parse_args():
@@ -36,12 +36,14 @@ def parse_args():
                              "Use variable as category indicator.")
     parser.add_argument("--mc", action="store_true",
                         help="Use jet fake estimation based on mc shapes.")
+    parser.add_argument("--variable-selection", default=None, type=str, nargs=1,
+                        help="Select final discriminator for shape creation.")
     return parser.parse_args()
 
 
 def setup_logging(output_file, level=logging.INFO):
     logger.setLevel(level)
-    logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
 
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
@@ -57,6 +59,7 @@ def main(args):
     input_file = ROOT.TFile(args.input)
 
     # Loop over histograms to extract relevant information for synced files.
+    logging.info("Reading input histograms from file %s", args.input)
     hist_map = {}
     for key in input_file.GetListOfKeys():
         split_name = key.GetName().split("#")
@@ -65,9 +68,22 @@ def main(args):
         if args.gof:
             # Use variable as category label for GOF test and control plots.
             category = split_name[3]
+            process = "-".join(split_name[1].split("-")[1:]) if not "data" in split_name[0] else "data_obs"
         else:
             category = split_name[1].split("-")[-1]
+            process = "-".join(split_name[1].split("-")[1:-1]) if not "data" in split_name[0] else "data_obs"
+            # Skip discriminant variables we do not want in the sync file.
+            # This is necessary because the sync file only allows for one type of histogram.
+            # A combination of the runs for different variables can then be used in separate files.
+            if args.variable_selection is None:
+                pass
+            else:
+                if split_name[3] not in args.variable_selection:
+                    continue
         variation = split_name[2]
+        # Skip variations necessary for estimations which are of no further use.
+        if "same_sign" in variation or "anti_iso" in variation:
+            continue
 
         # Check if channel and category are already in the map
         if not channel in hist_map:
@@ -76,7 +92,6 @@ def main(args):
             hist_map[channel][category] = {}
 
         _rev_process_map = {val: key for key, val in _process_map.items()}
-        process = "-".join(split_name[1].split("-")[1:-1]) if not "data" in split_name[0] else "data"
         # Skip copying of jetFakes estimations based on underlying shapes to be able
         # to use one name in the synced file.
         # TODO: Should this be kept or do we want to put both version in the synced file and
@@ -92,10 +107,18 @@ def main(args):
         name_output = "{process}".format(process=process)
         if "Nominal" not in variation:
             name_output += "_" + variation
+        logging.debug("Adding histogram with name %s as %s to category %s.",
+                      key.GetName(), name_output, channel + "_" + category)
         hist_map[channel][category][key.GetName()] = name_output
 
     # Loop over map and create the output file.
     for channel in hist_map:
+        logging.info("Writing histograms to file %s",
+                     os.path.join(
+                            args.output,
+                            "{ERA}-{CHANNELS}-synced-MSSM.root".format(
+                                                                    CHANNELS=channel,
+                                                                    ERA=args.era)))
         filename_output = os.path.join(
                 args.output, "{ERA}-{CHANNELS}-synced-MSSM.root".format(CHANNELS=channel, ERA=args.era))
         if not os.path.exists(args.output):
@@ -120,12 +143,14 @@ def main(args):
 
                 if "Era" in name_output:
                     if ("_1ProngPi0Eff_" in name_output
-                            or "_3ProngEff_" in name_output):
+                            or "_3ProngEff_" in name_output
+                            or "_dyShape_" in name_output):
                         hist.SetTitle(name_output.replace("_Era", ""))
                         hist.SetName(name_output.replace("_Era", ""))
                         hist.Write()
         output_file.Close()
 
+    logging.info("Successfully written all histograms to file.")
     # Clean up
     input_file.Close()
 
