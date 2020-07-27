f488603af0f18fa421bcbcace0061de046489885
Maximilian Burkart <maxiburkart@gmail.com>
2020-07-27 15:29:15 +0200

    Switch to latest privately compiled root version.

--- utils/setup_root.sh
+++ utils/setup_root.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
-source /home/wunsch/workspace/root/build_own_python/bin/thisroot.sh
+source /home/wunsch/workspace/root_v622/build_own_python/bin/thisroot.sh
 
 alias python=/home/wunsch/workspace/python/install/bin/python3.6
 export PYTHONPATH=$PWD:$PYTHONPATH
