f9d92f32e8a0f0d89128dba238ae8edfb53d65e7
Maximilian Burkart <maxiburkart@gmail.com>
2020-07-31 08:28:46 +0200

    Add utility script for estimations.

---
+++ shapes/do_estimations.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ERA=$1
+INPUT=$2
+EMBTT=$3
+[[ -z $EMBTT ]] && EMBTT=1
+echo $EMBTT
+
+if [[ $EMBTT == 1 ]]
+then
+    EMB_ARG="--emb-tt"
+else
+    EMB_ARG=""
+fi
+
+source utils/setup_root.sh
+
+python shapes/do_estimations.py -e $ERA -i $INPUT $EMB_ARG
+
+# Renormalize the fake factor shapes
+python fake-factor-application/normalize_shifts.py $INPUT
