41d0384238aee2ad4df8c0af9b76e48321620f99
Maximilian Burkart <maxiburkart@gmail.com>
2020-09-24 12:24:12 +0200

    Change interface of check script.

--- submit/check_outputs.sh
+++ submit/check_outputs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ERA=$1
-TAG=$2
-CHANNELS=${@:3}
+IFS="," read -r -a CHANNELS <<< $2
+TAG=$3
 
 source utils/setup_root.sh
 source utils/setup_susy_samples.sh $ERA
 
-python submit/check_outputs.py -e $ERA -c $CHANNELS --tag $TAG
+python submit/check_outputs.py -e $ERA -c ${CHANNELS[@]} --tag $TAG
