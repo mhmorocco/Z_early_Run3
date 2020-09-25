8c3b7824a0f66e5527fd97541cdb0b05e7c7c1ea
Maximilian Burkart <maxiburkart@gmail.com>
2020-09-25 14:24:06 +0200

    Add coloured output to convert script.

--- shapes/convert_to_synced_shapes.sh
+++ shapes/convert_to_synced_shapes.sh
@@ -5,14 +5,15 @@ CHANNEL=$2
 VARIABLE=$3
 TAG=$4
 
+source utils/bashFunctionCollection.sh
 source utils/setup_root.sh
 
-python shapes/convert_to_synced_shapes.py -e $ERA \
-                                          -i output/shapes/${ERA}-${CHANNEL}-analysis-shapes-${TAG}/shapes-analysis-${ERA}-${CHANNEL}.root \
-                                          -o output/shapes/${ERA}-${CHANNEL}-${TAG}-synced_shapes_${VARIABLE} \
-                                          --variable-selection ${VARIABLE} \
-                                          -n 12
+logandrun python shapes/convert_to_synced_shapes.py -e $ERA \
+                                                    -i output/shapes/${ERA}-${CHANNEL}-analysis-shapes-${TAG}/shapes-analysis-${ERA}-${CHANNEL}.root \
+                                                    -o output/shapes/${ERA}-${CHANNEL}-${TAG}-synced_shapes_${VARIABLE} \
+                                                    --variable-selection ${VARIABLE} \
+                                                    -n 12
 
 OUTFILE=output/shapes/${ERA}-${CHANNEL}-${TAG}-synced_shapes_${VARIABLE}.root
 echo "[INFO] Adding written files to single output file $OUTFILE..."
-hadd $OUTFILE output/shapes/${ERA}-${CHANNEL}-${TAG}-synced_shapes_${VARIABLE}/*.root
+logandrun hadd -f $OUTFILE output/shapes/${ERA}-${CHANNEL}-${TAG}-synced_shapes_${VARIABLE}/*.root
