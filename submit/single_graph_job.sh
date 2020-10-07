e622714d782e626561d0f594030d7b6546411622
Maximilian Burkart <maxiburkart@gmail.com>
2020-10-07 17:34:15 +0200

    Switch to root version from LCG stack.

--- submit/single_graph_job.sh
+++ submit/single_graph_job.sh
@@ -7,8 +7,6 @@ PROC_DIR=$3
 THREAD_ARG="--num-threads 1"
 [[ -z $4 ]] || THREAD_ARG="--num-threads $4"
 
-echo "ROOT version: `which root`"
-echo "python version: `which python`"
 echo "INPUT: $INPUT"
 echo "GRAPH: $GRAPH"
 echo "PROC_DIR: $PROC_DIR"
@@ -24,4 +22,5 @@ fi
 
 source utils/setup_root.sh
 
-/home/wunsch/workspace/python/install/bin/python3.6 submit/single_graph_job.py --input $INPUT --graph-number $GRAPH $THREAD_ARG
+echo "Running 'python submit/single_graph_job.py --input $INPUT --graph-number $GRAPH $THREAD_ARG'"
+python submit/single_graph_job.py --input $INPUT --graph-number $GRAPH $THREAD_ARG
--- utils/setup_root.sh
+++ utils/setup_root.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
-source /home/wunsch/workspace/root_v622/build_own_python/bin/thisroot.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_98python3/x86_64-centos7-gcc9-opt/setup.sh
 
-alias python=/home/wunsch/workspace/python/install/bin/python3.6
 export PYTHONPATH=$PWD:$PYTHONPATH
