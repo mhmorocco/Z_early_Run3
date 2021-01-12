aa2fc5d0cf7f4c7581be3576fa1ab6a5cb1f4aec
ralfschmieder <ralf.schmieder@student.kit.edu>
2021-01-12 15:57:10 +0100

    script to produce shapes with variations

--- produce_shapes_all.sh
+++ produce_shapes_all.sh
@@ -1,10 +1,17 @@
+ulimit -s unlimited
+ 
 for era in 2016 2017 2018
 do
-    for channel in et mt tt
+    for channel in  et mt tt
     do
         echo $era $channel
-        python shapes/produce_shapes.py --channels ${channel} --output-file output/shapes/${era}-${channel}-control-shapes/shapes-analysis-${era}-${channel} --directory /ceph/jbechtel/nmssm/ntuples/${era}/${channel}/ --${channel}-friend-directory /ceph/jbechtel/nmssm/friends/${era}/${channel}/SVFit/ /ceph/jbechtel/nmssm/friends/${era}/${channel}/FakeFactors_nmssm/ /ceph/jbechtel/nmssm/friends/${era}/${channel}/HHKinFit/ /ceph/jbechtel/nmssm/friends/${era}/${channel}/NNScore_retrain_cutonChi2/NNScore_workdir/NNScore_collected/ --era ${era} --num-processes 4 --num-threads 3 --optimization-level 1 --control-plots --skip-systematic-variations --control-plot-set mjj,pt_1,pt_2,m_vis,ptvis,m_sv_puppi,jpt_1,njets,jdeta,mjj,dijetpt,bpt_bReg_1,bpt_bReg_2,jpt_2,mbb_highCSV_bReg,pt_bb_highCSV_bReg,m_ttvisbb_highCSV_bReg,kinfit_mH,kinfit_mh2,kinfit_chi2,nbtag,bm_bReg_1,bm_bReg_2,bcsv_1,bcsv_2,highCSVjetUsedFordiBJetSystemCSV
-        bash shapes/do_estimations.sh ${era} output/shapes/${era}-${channel}-control-shapes/shapes-analysis-${era}-${channel}.root 0
+        mkdir output/uncert_shapes/${era}-${channel}-control-shapes
+        python shapes/produce_shapes.py --channels ${channel} --output-file output/uncert_shapes/${era}-${channel}-control-shapes/shapes-analysis-${era}-${channel} --directory /ceph/jbechtel/nmssm/ntuples/${era}/${channel}/ --${channel}-friend-directory /ceph/jbechtel/nmssm/friends/${era}/${channel}/SVFit/ /ceph/rschmieder/nmssm/friends/${era}/${channel}/FakeFactors_nmssm/ /ceph/jbechtel/nmssm/friends/${era}/${channel}/HHKinFit/ /ceph/jbechtel/nmssm/friends/${era}/${channel}/NNScore_train_all/NNScore_workdir/500_3/NNScore_workdir/NNScore_collected/ --era ${era} --num-processes 5 --num-threads 4 --optimization-level 1 --control-plot-set ${channel}_max_score 
+
+        bash shapes/do_estimations.sh ${era} output/uncert_shapes/${era}-${channel}-control-shapes/shapes-analysis-${era}-${channel}.root 0
+
+        python shapes/convert_to_synced_shapes.py --era ${era} --input output/uncert_shapes/${era}-${channel}-control-shapes/shapes-analysis-${era}-${channel}.root --output output/uncert_shapes/synced_shapes
+        
+        hadd output/uncert_shapes/synced_shapes/${era}-${channel}-synced-NMSSM.root output/uncert_shapes/synced_shapes/${era}-${channel}-synced-NMSSM*
     done
 done
-
