aec33320d5358a6cfa4709db74959696a60a2ee6
ralfschmieder <ralf.schmieder@student.kit.edu>
2021-03-03 11:43:18 +0100

    update

--- submit/prepare_args_file.py
+++ submit/prepare_args_file.py
@@ -11,13 +11,15 @@ def parse_args():
                         help="The input file containing the graphs to be produced.")
     parser.add_argument("-o", "--output-dir",
                         help="The output directory the arguments file is written to.")
+    parser.add_argument("--pack-multiple-pipelines", default=None, type=int,
+                        help="Run given number of pipelines in one job.")
     return parser.parse_args()
 
 
-def write_file(output_dir, graph_file, num_jobs, workdir):
+def write_file(output_dir, graph_file, job_nums, workdir):
     tmp_string = "{directory} {job_num} {file}\n"
     with open(os.path.join(output_dir, "arguments.txt"), "w") as f:
-        for job_num in num_jobs:
+        for job_num in job_nums:
             f.write(tmp_string.format(file=graph_file,
                                       job_num=job_num,
                                       directory=workdir))
@@ -51,12 +53,35 @@ def split_multicore_jobs(graphs):
     return single_ind, mult_ind
 
 
+def prepare_multigraph_jobs(graph_inds, group_size):
+    # Get loop index representing number of jobs after grouping minus one.
+    num_jobs = len(graph_inds) // group_size
+    grouped_inds = []
+    for i in range(num_jobs):
+        grouped_inds.append("{}-{}".format(graph_inds[i*group_size],
+                                           graph_inds[i*group_size+group_size-1]
+                                           ))
+    # Check if only one graph remains. If more than one job remains
+    if (len(graph_inds) % group_size) == 0:
+        pass
+    elif (len(graph_inds) % group_size) == 1:
+        grouped_inds.append("{}".format(graph_inds[-1]))
+    else:
+        grouped_inds.append("{}-{}".format(graph_inds[(num_jobs)*group_size],
+                                           graph_inds[-1]
+                                           ))
+    return grouped_inds
+
 
 def main(args):
     with open(args.graph_file, "rb") as f:
         graphs = pickle.load(f)
     workdir = os.getcwd()
     num_singles, num_multi = split_multicore_jobs(graphs)
+    if args.pack_multiple_pipelines is None:
+        pass
+    else:
+        num_singles = prepare_multigraph_jobs(list(num_singles), args.pack_multiple_pipelines)
     write_file(args.output_dir, args.graph_file, num_singles, workdir)
     write_file_multicore(args.output_dir, args.graph_file, num_multi, workdir)
     return
