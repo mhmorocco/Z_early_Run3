49ed01e54f68ae6ae67955ae8651b6834cf5dc59
ralfschmieder <ralf.schmieder@student.kit.edu>
2021-03-03 11:44:45 +0100

    update

--- submit/single_graph_job.py
+++ submit/single_graph_job.py
@@ -13,7 +13,7 @@ logger = logging.getLogger("")
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", type=str, help="Input file containing the pickled graphs")
-    parser.add_argument("-g", "--graph-number", type=int, help="Number of the graph to be processed.")
+    parser.add_argument("-g", "--graph-number", type=str, help="Number of the graph to be processed.")
     parser.add_argument("-n", "--num-threads", type=int, default=1,
                         help="Number of threads to run on.")
     parser.add_argument("--output", type=str, default=None,
@@ -33,24 +33,30 @@ def setup_logging(output_file, level=logging.DEBUG):
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
-
 def main(args):
 
     with open(args.input, "rb") as f:
         graphs = pickle.load(f)
-        graph_to_process = graphs[args.graph_number]
+        if "-" in args.graph_number:
+            graph_ind = list(map(int, args.graph_number.split("-")))
+            graph_to_process = graphs[graph_ind[0]:graph_ind[1]+1]
+        else:
+            graph_to_process = [graphs[int(args.graph_number)]]
     logger.info("Processing graph number {} out of {} graphs.".format(args.graph_number, len(graphs)))
 
-    logger.info(graphs[args.graph_number])
+    if "-" in args.graph_number:
+        logger.info(graphs[graph_ind[0]:graph_ind[1]+1])
+    else:
+        logger.info(graphs[int(args.graph_number)])
     if args.output is None:
         output_file = os.path.join("output/shapes", os.path.basename(args.input).replace(".pkl", ""), "output-single_graph_job-{}-{}.root".format(
                                 os.path.basename(args.input.replace(".pkl", "")),
-                                args.graph_number))
+                                args.graph_number.replace("-", "_")))
     else:
         output_file = args.output
 
     # Step 3: convert to RDataFrame and run the event loop
-    r_manager = RunManager([graph_to_process])
+    r_manager = RunManager(graph_to_process)
     r_manager.run_locally(output_file, 1, args.num_threads)
 
     return
@@ -62,5 +68,5 @@ if __name__ == "__main__":
     setup_logging(os.path.join(pathname, "single_graph_job-{id}-{num}.log".format(
                                         id=os.path.basename(args.input).replace(".pkl", ""),
                                         num=args.graph_number)),
-                  level=logging.INFO)
+                  level=logging.DEBUG)
     main(args)
