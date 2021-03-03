adf724bd28337144a966ceb4f6ee60cb18ace010
ralfschmieder <ralf.schmieder@student.kit.edu>
2021-03-03 11:45:40 +0100

    paths updated

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -2,54 +2,67 @@
 set -e
 
 ERA=$1
+CHANNEL=$2
+
+# if [[ ! -z $3 ]]; then
+#     OUTPUTDIR=$3
+# elif [[ -d output/friend_trees ]];then
+#     OUTPUTDIR=$( cd output; pwd -P)
+# fi
+
+# Kappa database
+
+#KAPPA_DATABASE=datasets/datasets.json
 
 #### ERA specific part. If a sample is not available comment it out here.
 # Samples Run2016
-basedir="/ceph/mburkart/Run2Legacy/ntuples/MSSM_Legacy/MSSM_2020_04_27_UseOfSMLegacy"
-ARTUS_OUTPUTS_2016="$basedir/2016/ntuples/"
-SVFit_Friends_2016="$basedir/2016/friends/SVFit/"
-FF_Friends_2016="$basedir/2016/friends/FakeFactors/"
-EMQCD_Friends_2016="$basedir/2016/friends/EMQCDWeights/"
-
+ARTUS_OUTPUTS_2016="/ceph/jbechtel/nmssm/ntuples/2016/${CHANNEL}/"
+SVFit_Friends_2016="/ceph/rschmieder/nmssm/friends/2016/${CHANNEL}/SVFit/"
+FF_Friends_2016="/ceph/rschmieder/nmssm/friends/2016/${CHANNEL}/FakeFactors_nmssm/"
+HHKinFit_Friends_2016="/ceph/rschmieder/nmssm/friends/2016/${CHANNEL}/HHKinFit/"
+NNScore_Friends_2016="/ceph/rschmieder/nmssm/friends/2016/${CHANNEL}/NNScore_train_all/68_onenet_summed_score/NNScore_workdir/NNScore_collected/"
+#NNScore_Friends_2016="/ceph/rschmieder/nmssm/friends/2016/${CHANNEL}/NNScore_train_all/parametrized_nn_mH1000/NNScore_workdir/NNScore_collected/"
 # Samples Run2017
-ARTUS_OUTPUTS_2017="$basedir/2017/ntuples/"
-SVFit_Friends_2017="$basedir/2017/friends/SVFit/"
-FF_Friends_2017="$basedir/2017/friends/FakeFactors/"
-EMQCD_Friends_2017="$basedir/2017/friends/EMQCDWeights/"
-
+ARTUS_OUTPUTS_2017="/ceph/jbechtel/nmssm/ntuples/2017/${CHANNEL}/"
+SVFit_Friends_2017="/ceph/rschmieder/nmssm/friends/2017/${CHANNEL}/SVFit/"
+FF_Friends_2017="/ceph/rschmieder/nmssm/friends/2017/${CHANNEL}/FakeFactors_nmssm/"
+HHKinFit_Friends_2017="/ceph/rschmieder/nmssm/friends/2017/${CHANNEL}/HHKinFit/"
+NNScore_Friends_2017="/ceph/rschmieder/nmssm/friends/2017/${CHANNEL}/NNScore_train_all/68_onenet_summed_score/NNScore_workdir/NNScore_collected/"
+#NNScore_Friends_2017="/ceph/rschmieder/nmssm/friends/2017/${CHANNEL}/NNScore_train_all/parametrized_nn_mH1000/NNScore_workdir/NNScore_collected/"
 # Samples Run2018
-ARTUS_OUTPUTS_2018="$basedir/2018/ntuples/"
-SVFit_Friends_2018="$basedir/2018/friends/SVFit/"
-FF_Friends_2018="$basedir/2018/friends/FakeFactors/"
-EMQCD_Friends_2018="$basedir/2018/friends/EMQCDWeights/"
-
+ARTUS_OUTPUTS_2018="/ceph/jbechtel/nmssm/ntuples/2018/${CHANNEL}/"
+SVFit_Friends_2018="/ceph/rschmieder/nmssm/friends/2018/${CHANNEL}/SVFit/"
+FF_Friends_2018="/ceph/rschmieder/nmssm/friends/2018/${CHANNEL}/FakeFactors_nmssm/"
+HHKinFit_Friends_2018="/ceph/rschmieder/nmssm/friends/2018/${CHANNEL}/HHKinFit/"
+NNScore_Friends_2018="/ceph/rschmieder/nmssm/friends/2018/${CHANNEL}/NNScore_train_all/68_onenet_summed_score/NNScore_workdir/NNScore_collected/"
+#NNScore_Friends_2018="/ceph/rschmieder/nmssm/friends/2018/${CHANNEL}/NNScore_train_all/parametrized_nn_mH1000/NNScore_workdir/NNScore_collected/"
 
 # ERA handling
 if [[ $ERA == *"2016"* ]]
 then
     ARTUS_OUTPUTS=$ARTUS_OUTPUTS_2016
+    NNScore_Friends=$NNScore_Friends_2016
     SVFit_Friends=$SVFit_Friends_2016
+    MELA_Friends=$MELA_Friends_2016
     FF_Friends=$FF_Friends_2016
-    EMQCD_Friends=$EMQCD_Friends_2016
+    HHKinFit_Friends=$HHKinFit_Friends_2016
 elif [[ $ERA == *"2017"* ]]
 then
     ARTUS_OUTPUTS=$ARTUS_OUTPUTS_2017
+    NNScore_Friends=$NNScore_Friends_2017
     SVFit_Friends=$SVFit_Friends_2017
+    MELA_Friends=$MELA_Friends_2017
     FF_Friends=$FF_Friends_2017
-    EMQCD_Friends=$EMQCD_Friends_2017
+    HHKinFit_Friends=$HHKinFit_Friends_2017
 elif [[ $ERA == *"2018"* ]]
 then
     ARTUS_OUTPUTS=$ARTUS_OUTPUTS_2018
+    NNScore_Friends=$NNScore_Friends_2018
     SVFit_Friends=$SVFit_Friends_2018
+    MELA_Friends=$MELA_Friends_2018
     FF_Friends=$FF_Friends_2018
-    EMQCD_Friends=$EMQCD_Friends_2018
+    HHKinFit_Friends=$HHKinFit_Friends_2018
 fi
 
-### channels specific friend tree.
-# Used for example to process the event channel without including the fakefactor friends
-ARTUS_FRIENDS_EM="$SVFit_Friends $EMQCD_Friends"
-ARTUS_FRIENDS_ET="$SVFit_Friends"
-ARTUS_FRIENDS_MT="$SVFit_Friends"
-ARTUS_FRIENDS_TT="$SVFit_Friends"
-ARTUS_FRIENDS="$SVFit_Friends"
 ARTUS_FRIENDS_FAKE_FACTOR=$FF_Friends
+
