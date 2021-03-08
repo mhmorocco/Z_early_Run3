52b01279f38b683db9df6d0a7e57424d9224f1fe
ralfschmieder <ralf.schmieder@student.kit.edu>
2021-03-08 10:16:53 +0100

    outcommented normalize_shifts

--- shapes/do_estimations.sh
+++ shapes/do_estimations.sh
@@ -18,4 +18,4 @@ source utils/setup_root.sh
 python shapes/do_estimations.py -e $ERA -i $INPUT $EMB_ARG
 
 # Renormalize the fake factor shapes
-python fake-factor-application/normalize_shifts.py $INPUT
+#python fake-factor-application/normalize_shifts.py $INPUT
